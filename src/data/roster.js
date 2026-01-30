8f555524440bec416b1443561be88be59585d1f9
Jon Soll <jsoll43@gmail.com>
2026-01-30 17:13:50 -0500

    Add players A-E to roster

--- src/data/roster.js
+++ src/data/roster.js
@@ -1,5 +1,7 @@
-export const roster = [
-  { id: "1", number: 1, first: "First", last: "Player" },
-  { id: "2", number: 2, first: "Second", last: "Player" },
+export const roster = [  { id: "player-a", number: "A", first: "Player", last: "A" },
+  { id: "player-b", number: "B", first: "Player", last: "B" },
+  { id: "player-c", number: "C", first: "Player", last: "C" },
+  { id: "player-d", number: "D", first: "Player", last: "D" },
+  { id: "player-e", number: "E", first: "Player", last: "E" },
   // TODO: replace with your real roster
 ];
