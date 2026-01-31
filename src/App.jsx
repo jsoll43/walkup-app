a922107172ce9f56fea9dd7020d02e6e69043752
Jon Soll <jsoll43@gmail.com>
2026-01-30 22:42:21 -0500

    Add parent login route and standardize parent paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import ParentRecord from "./pages/ParentRecord";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+
 import ParentHome from "./pages/ParentHome";
+import ParentRecord from "./pages/ParentRecord";
+import ParentLogin from "./pages/ParentLogin";
+
 import Coach from "./pages/Coach";
 import Admin from "./pages/Admin";
 
@@ -8,16 +11,26 @@ export default function App() {
   return (
     <BrowserRouter>
       <div style={{ padding: 16, borderBottom: "1px solid #ddd" }}>
-        <Link to="/" style={{ marginRight: 12 }}>Parent</Link>
+        <Link to="/parent" style={{ marginRight: 12 }}>Parent</Link>
         <Link to="/coach" style={{ marginRight: 12 }}>Coach</Link>
         <Link to="/admin">Admin</Link>
       </div>
 
       <Routes>
-        <Route path="/record/:playerId" element={<ParentRecord />} />
-        <Route path="/" element={<ParentHome />} />
+        {/* Default: send / to /parent */}
+        <Route path="/" element={<Navigate to="/parent" replace />} />
+
+        {/* Parent privacy gate */}
+        <Route path="/parent-login" element={<ParentLogin />} />
+        <Route path="/parent" element={<ParentHome />} />
+        <Route path="/parent/:playerId" element={<ParentRecord />} />
+
+        {/* Coach/Admin */}
         <Route path="/coach" element={<Coach />} />
         <Route path="/admin" element={<Admin />} />
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/parent" replace />} />
       </Routes>
     </BrowserRouter>
   );
