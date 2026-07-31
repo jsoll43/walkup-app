70e7974ccb8fdcbb74ef11e03333c9af0efdd6cd
Jon Soll <jsoll43@gmail.com>
2026-07-30 21:28:08 -0400

    added seasons

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames(assetInfo) {
+          if (assetInfo.name?.endsWith(".css")) return "assets/app.css";
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
   server: {
     host: true,
   },
